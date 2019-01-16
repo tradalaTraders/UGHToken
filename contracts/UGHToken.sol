bb2b0f0d96fbe941c682b919b034231f67997cef
Vikas <vikas@tradala.in>
2019-01-16 21:33:05 +0530

    message

--- contracts/UGHToken.sol
+++ contracts/UGHToken.sol
@@ -4,17 +4,17 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract UGHToken is ERC20,ERC20Detailed, Ownable {
-    uint public INITIAL_SUPPLY = 40000000;
-    string public _name = "UGH Token";
-    string public _symbol = "UGH";
-    uint public _decimals = 18;
+contract UGHToken is ERC20, ERC20Detailed, Ownable {
+    uint public constant INITIAL_SUPPLY =  40000000;
+    uint8 public constant DECIMALS = 18;
     mapping (address => uint256) private _balances;
 
     address _manager;
-    constructor (address _managerAddress) ERC20Detailed(_name,_symbol, 18) public{
+    constructor (address _managerAddress) ERC20Detailed("UGH Token","UGH", DECIMALS) public{
         _mint(msg.sender, INITIAL_SUPPLY);
+        require(_managerAddress!=address(0));
         _balances[_managerAddress] = INITIAL_SUPPLY * 18; 
+        // emit Transfer(0x0, _managerAddress, INITIAL_SUPPLY);
     }
      function () payable external { 
          revert();
